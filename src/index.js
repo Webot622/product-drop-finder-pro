4f02b4eccae54ed513d52437b26b70a747625307
Webot622 <webot622@gmail.com>
2025-12-29 11:55:15 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+```javascript
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+
+reportWebVitals();
+```
